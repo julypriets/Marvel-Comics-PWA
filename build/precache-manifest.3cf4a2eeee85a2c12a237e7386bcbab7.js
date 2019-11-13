61f677ec5aca7613c30263b96d615a10214b5abf
Juliana Prieto <jprietoarcila@gmail.com>
2019-11-13 09:29:38 -0500

    Update

---
+++ build/precache-manifest.3cf4a2eeee85a2c12a237e7386bcbab7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bd643b38c4c525af07da01574d7d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "141942911fd379505450",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6ce477e8edffea7a2008",
+    "url": "/static/js/2.d01e1fd2.chunk.js"
+  },
+  {
+    "revision": "141942911fd379505450",
+    "url": "/static/js/main.2fd6abfd.chunk.js"
+  },
+  {
+    "revision": "13b80cbef186b935ad92",
+    "url": "/static/js/runtime-main.42a56e01.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
